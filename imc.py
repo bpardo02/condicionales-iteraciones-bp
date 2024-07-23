db1bc2b3c56a4a038a353efd817f027cefe32ffa
Benjamin <pardobenjamin.r@gmail.com>
2024-07-23 19:08:21 -0400

    añadir print de imc

--- imc.py
+++ imc.py
@@ -1,20 +1,26 @@
 #datos a ingresar por usuario
 peso = float(input('Ingrese peso en kilogramos (kg): '))
-altura = float(input('Ingrese altura en metros (m)'))
+altura = float(input('Ingrese altura en metros (m): '))
 
 #formula
 imc = peso/(altura**2)
 
 #condiciones
 if imc<18.5:
+    print:(f"su indice de masa corporal es: {imc}")
     print("El resultado del imc es: bajo peso")
 elif 18.5<=imc<25:
+    print:(f"su indice de masa corporal es: {imc}")
     print("El resultado del imc es: peso normal")
 elif 25<=imc<30:
+    print:(f"su indice de masa corporal es: {imc}")
     print("El resultado del imc es: sobrepeso")
 elif 30<=imc<35:
+    print:(f"su indice de masa corporal es: {imc}")
     print("El resultado del imc es: obesidad grado I")
 elif 35<=imc<40:
+    print:(f"su indice de masa corporal es: {imc}")
     print("EL resultado del imc es: obesidad grado II")
 else:
+    print:(f"su indice de masa corporal es: {imc}")
     print("El resultado del imc es: obesidad grado III")
