1584543eb666ff922c82bc0bed727969cdbc9b8a
Benjamin <pardobenjamin.r@gmail.com>
2024-07-23 19:09:39 -0400

    corregido un error de print

--- imc.py
+++ imc.py
@@ -7,20 +7,20 @@ imc = peso/(altura**2)
 
 #condiciones
 if imc<18.5:
-    print:(f"su indice de masa corporal es: {imc}")
+    print(f"su indice de masa corporal es: {imc}")
     print("El resultado del imc es: bajo peso")
 elif 18.5<=imc<25:
-    print:(f"su indice de masa corporal es: {imc}")
+    print(f"su indice de masa corporal es: {imc}")
     print("El resultado del imc es: peso normal")
 elif 25<=imc<30:
-    print:(f"su indice de masa corporal es: {imc}")
+    print(f"su indice de masa corporal es: {imc}")
     print("El resultado del imc es: sobrepeso")
 elif 30<=imc<35:
-    print:(f"su indice de masa corporal es: {imc}")
+    print(f"su indice de masa corporal es: {imc}")
     print("El resultado del imc es: obesidad grado I")
 elif 35<=imc<40:
-    print:(f"su indice de masa corporal es: {imc}")
+    print(f"su indice de masa corporal es: {imc}")
     print("EL resultado del imc es: obesidad grado II")
 else:
-    print:(f"su indice de masa corporal es: {imc}")
+    print(f"su indice de masa corporal es: {imc}")
     print("El resultado del imc es: obesidad grado III")
